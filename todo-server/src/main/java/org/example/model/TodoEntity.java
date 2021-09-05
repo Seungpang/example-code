cbecc0da412278fb9cb7573591505c0700f83daf
Seungpang <obey1342@gmail.com>
2021-09-05 17:11:23 +0900

    model 구현

---
+++ todo-server/src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,30 @@
+package org.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "todoOrder", nullable = false)
+    private Long order;
+
+    @Column(nullable = false)
+    private Boolean completed;
+}

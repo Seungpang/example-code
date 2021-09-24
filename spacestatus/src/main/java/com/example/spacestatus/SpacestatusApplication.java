0c81cd3e6bcfcafb66228c14e1547e4c9fc051b7
Kim Seunglae <obey1342@gmail.com>
2021-09-24 23:39:01 +0900

    init

---
+++ spacestatus/src/main/java/com/example/spacestatus/SpacestatusApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacestatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacestatusApplication.class, args);
+	}
+
+}
---
+++ spacestatus/src/test/java/com/example/spacestatus/SpacestatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacestatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

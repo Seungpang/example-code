bf25ef12422b659cc7a7ebde8036f588c1e94b78
Kim Seunglae <obey1342@gmail.com>
2021-10-01 12:43:06 +0900

    init project

---
+++ test-spring-batch/src/main/java/com/seungpang/testspringbatch/TestSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.seungpang.testspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBatchApplication.class, args);
+	}
+
+}

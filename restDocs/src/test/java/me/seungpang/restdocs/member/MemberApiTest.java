81a2593478ef2e6fc0b49a3326dd0f710e7f3108
Kim Seunglae <obey1342@gmail.com>
2021-10-19 17:07:20 +0900

    테스트 코드 개선

--- restDocs/src/test/java/me/seungpang/restdocs/member/MemberApiTest.java
+++ restDocs/src/test/java/me/seungpang/restdocs/member/MemberApiTest.java
@@ -3,41 +3,13 @@ package me.seungpang.restdocs.member;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
+import me.seungpang.restdocs.TestSupport;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(RestDocumentationExtension.class)
-class MemberApiTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(
-        final WebApplicationContext context,
-        final RestDocumentationContextProvider provider
-    ) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-            .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
-            .build();
-    }
+class MemberApiTest extends TestSupport {
 
     @Test
     void member_page_test() throws Exception {
@@ -47,8 +19,6 @@ class MemberApiTest {
                     .param("page", "0")
                     .contentType(MediaType.APPLICATION_JSON)
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk());
     }
 
@@ -58,8 +28,6 @@ class MemberApiTest {
                 get("/api/members/{id}",1L)
                     .contentType(MediaType.APPLICATION_JSON)
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk());
     }
 
@@ -68,13 +36,8 @@ class MemberApiTest {
         mockMvc.perform(
                 post("/api/members")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content("{\n"
-                        + "  \"email\": \"obey1342@gmail.com\",\n"
-                        + "  \"name\": \"seungpang\"\n"
-                        + "}")
+                    .content(readJson("/json/member-api/member-create.json"))
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk());
     }
 
@@ -83,12 +46,8 @@ class MemberApiTest {
         mockMvc.perform(
                 put("/api/members/{id}",1L)
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content("{\n"
-                        + "  \"name\": \"seung\"\n"
-                        + "}")
+                    .content(readJson("/json/member-api/member-modify.json"))
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk());
     }
 }

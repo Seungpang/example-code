bcd694675a7bd2a9c70125f3319e4f51b7486f65
Kim Seunglae <obey1342@gmail.com>
2022-01-22 23:20:08 +0900

    오류 처리

--- house-utils/src/main/java/com/example/houseutils/policy/BrokeragePolicyFactory.java
+++ house-utils/src/main/java/com/example/houseutils/policy/BrokeragePolicyFactory.java
@@ -1,17 +1,19 @@
 package com.example.houseutils.policy;
 
 import com.example.houseutils.constants.ActionType;
+import com.example.houseutils.exception.ErrorCode;
+import com.example.houseutils.exception.HouseUtilsException;
 
 public class BrokeragePolicyFactory {
 
-    public static BrokeragePolicy of(ActionType actionType) {
-        switch (actionType) {
-            case RENT:
-                return new RentBrokeragePolicy();
-            case PURCHASE:
-                return new PurchaseBrokeragePolicy();
-            default:
-                throw new IllegalArgumentException("해당 actionType에 대한 정책이 존재하지 않습니다.");
-        }
-    }
+	public static BrokeragePolicy of(ActionType actionType) {
+		switch (actionType) {
+			case RENT:
+				return new RentBrokeragePolicy();
+			case PURCHASE:
+				return new PurchaseBrokeragePolicy();
+			default:
+				throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actionType에 대한 정책이 존재하지 않습니다.");
+		}
+	}
 }

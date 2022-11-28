fa78276f1ec8039f98ba83c020478eadddad37f4
Kim Seunglae <obey1342@gmail.com>
2022-11-28 16:17:02 +0900

    synchronized 사용

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -14,8 +14,21 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+//    public void decrease(Long id, Long quantity) {
+//        Stock stock = stockRepository.findById(id)
+//                .orElseThrow();
+//
+//        stock.decrease(quantity);
+//
+//        stockRepository.saveAndFlush(stock);
+//    }
+
+    /**
+     * synchronized는 각 프로세스안에서만 보장
+     * 서버가 여러대일때 레이스 컨디션 그대로 발생
+     */
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id)
                 .orElseThrow();
 
--- stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,5 +42,31 @@ class StockServiceTest {
                 .orElseThrow();
 
         assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        final CountDownLatch latch = new CountDownLatch(threadCount);
+
+        //레이스컨디션 발생
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+
+            });
+        }
+
+        latch.await();
+
+        final Stock stock = stockRepository.findById(1L)
+                .orElseThrow();
+
+        assertThat(stock.getQuantity()).isZero();
     }
 }

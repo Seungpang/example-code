2c91e21e13b775443dfd1dcf32b4a06cd9af8354
Kim Seunglae <obey1342@gmail.com>
2022-11-28 17:24:10 +0900

    Named Lock 활용

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -14,21 +15,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-//    @Transactional
-//    public void decrease(Long id, Long quantity) {
-//        Stock stock = stockRepository.findById(id)
-//                .orElseThrow();
-//
-//        stock.decrease(quantity);
-//
-//        stockRepository.saveAndFlush(stock);
-//    }
-
-    /**
-     * synchronized는 각 프로세스안에서만 보장
-     * 서버가 여러대일때 레이스 컨디션 그대로 발생
-     */
-    public synchronized void decrease(Long id, Long quantity) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id)
                 .orElseThrow();
 
@@ -36,4 +24,17 @@ public class StockService {
 
         stockRepository.saveAndFlush(stock);
     }
+
+    /**
+     * synchronized는 각 프로세스안에서만 보장
+     * 서버가 여러대일때 레이스 컨디션 그대로 발생
+     */
+//    public synchronized void decrease(Long id, Long quantity) {
+//        Stock stock = stockRepository.findById(id)
+//                .orElseThrow();
+//
+//        stock.decrease(quantity);
+//
+//        stockRepository.saveAndFlush(stock);
+//    }
 }

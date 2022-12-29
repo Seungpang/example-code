322f80f656debdaa9473f9ad8ad2374d8249cd76
Kim Seunglae <obey1342@gmail.com>
2022-12-29 21:29:48 +0900

    project init

---
+++ redis/src/test/java/me/seungpang/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungpang.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

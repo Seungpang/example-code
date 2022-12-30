b822787cf0641d2e01aa7df926fb253160a6778a
Kim Seunglae <obey1342@gmail.com>
2022-12-30 18:06:27 +0900

    간단한 redis cache-Aside 적용

--- redis/src/main/java/me/seungpang/redis/service/UserService.java
+++ redis/src/main/java/me/seungpang/redis/service/UserService.java
@@ -1,7 +1,10 @@
 package me.seungpang.redis.service;
 
+import java.util.concurrent.TimeUnit;
 import me.seungpang.redis.dto.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +13,21 @@ public class UserService {
     @Autowired
     private ExternalApiService externalApiService;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     public UserProfile getUserProfile(String userId) {
-        String userName = externalApiService.getUserName(userId);
+        String userName = null;
+
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String cacheName = ops.get("nameKey:" + userId);
+        if (cacheName != null) {
+            userName = cacheName;
+        } else {
+            userName = externalApiService.getUserName(userId);
+            ops.set("nameKey:" + userId, userName, 5, TimeUnit.SECONDS);
+        }
+        //String userName = externalApiService.getUserName(userId);
         int userAge = externalApiService.getUserAge(userId);
         return new UserProfile(userName, userAge);
     }

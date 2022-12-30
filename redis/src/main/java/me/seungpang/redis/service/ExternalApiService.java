9b65e4641ac338ee6d0ce28519bbcb0879557acd
Kim Seunglae <obey1342@gmail.com>
2022-12-30 18:35:43 +0900

    spring의 캐싱 기능을 적용

--- redis/src/main/java/me/seungpang/redis/service/ExternalApiService.java
+++ redis/src/main/java/me/seungpang/redis/service/ExternalApiService.java
@@ -1,5 +1,6 @@
 package me.seungpang.redis.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +8,7 @@ public class ExternalApiService {
 
     public String getUserName(String userId) {
         try {
-            Thread.sleep(500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
         }
 
@@ -21,9 +22,10 @@ public class ExternalApiService {
         return "";
     }
 
+    @Cacheable(cacheNames = "userAgeCache", key = "#userId")
     public int getUserAge(String userId) {
         try {
-            Thread.sleep(500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
         }
 

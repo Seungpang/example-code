7fcd251249d703e742aa66cebea1bc9fce466a99
Kim Seunglae <obey1342@gmail.com>
2023-01-31 01:47:34 +0900

    api 테스트

--- product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
+++ product-order-service/src/main/java/com/example/productorderservice/product/ProductService.java
@@ -1,9 +1,14 @@
 package com.example.productorderservice.product;
 
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 class ProductService {
 
     private final ProductPort productPort;
@@ -12,10 +17,12 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    @Transactional
-    public void addProduct(final AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
---
+++ product-order-service/src/test/java/com/example/productorderservice/ApiTest.java
@@ -0,0 +1,19 @@
+package com.example.productorderservice;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+}

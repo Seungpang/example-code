543f73f15e4aa8c50c4183323adac69b4bb6abea
Kim Seunglae <obey1342@gmail.com>
2023-02-04 01:56:30 +0900

    Component 스캔 사용

--- helloboot/src/main/java/com/example/helloboot/SimpleHelloService.java
+++ helloboot/src/main/java/com/example/helloboot/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package com.example.helloboot;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(final String name) {

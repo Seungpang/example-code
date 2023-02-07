5492e06cb2fab9764b7765bc4b5775615e3fbfac
Kim Seunglae <obey1342@gmail.com>
2023-02-07 14:51:27 +0900

    Jetty 서버 구성 추가

--- helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package com.example.config.autoconfig;
 
 import com.example.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }

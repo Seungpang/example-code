24c3d2b10eebd2a2701a48e9fa34dced185f3baa
Kim Seunglae <obey1342@gmail.com>
2023-02-08 22:19:35 +0900

    Conditional 애노테이션  사용

--- helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,27 @@
 package com.example.config.autoconfig;
 
 import com.example.config.MyAutoConfiguration;
+import com.example.config.autoconfig.JettyWebServerConfig.JettyCondition;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }

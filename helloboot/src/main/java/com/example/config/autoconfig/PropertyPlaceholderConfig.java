8ec138388a492e88614316d3308b14dbc035a164
Kim Seunglae <obey1342@gmail.com>
2023-02-09 18:00:39 +0900

    PropertySourcesPlaceholderConfigurer 적용

---
+++ helloboot/src/main/java/com/example/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package com.example.config.autoconfig;
+
+import com.example.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- helloboot/src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,22 +2,27 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+
+        System.out.println(this.contextPath);
+
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }

7df9d83d53f29fb596263ad0279531fce119a962
Kim Seunglae <obey1342@gmail.com>
2023-02-22 16:12:51 +0900

    데이터 클래스

---
+++ kotlin/src/main/kotlin/DataClass.kt
@@ -0,0 +1,26 @@
+data class Person(val name: String, val age: Int) {
+}
+
+
+fun main() {
+    val person1 = Person(name = "seungpang", age = 20)
+
+    println("이름=${person1.component1()}, 나이=${person1.component2()}")
+
+    val (name, age) = person1
+
+    println("이름=${name}, 나이=${age}")
+
+    val person2 = person1.copy(name = "pang")
+    println(person2)
+//    println(person1.toString())
+//    println(person1 == person2)
+
+//    val set = hashSetOf(person1)
+//    println(set.contains(person2))
+
+//    person1.name = "strange"
+//    println(set.contains(person1))
+}
+
+

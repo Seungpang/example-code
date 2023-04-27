173410c85a32b5baa1b522f5f343e88683a9221e
Kim Seunglae <obey1342@gmail.com>
2023-04-27 22:48:08 +0900

    project init

---
+++ atdd-hexagonal/atdd-hexagonal/src/main/java/me/seungpang/bank/AtddHexagonalApplication.java
@@ -0,0 +1,13 @@
+package me.seungpang.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddHexagonalApplication.class, args);
+    }
+
+}

f521b4df4a893bf79142e3847040400a038bc040
Seungrae <obey1342@gmail.com>
2023-06-07 21:50:51 +0900

    project init

---
+++ kotest-practice/src/main/kotlin/me/seungpang/kotestpractice/KotestPracticeApplication.kt
@@ -0,0 +1,11 @@
+package me.seungpang.kotestpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotestPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotestPracticeApplication>(*args)
+}
---
+++ kotest-practice/src/test/kotlin/me/seungpang/kotestpractice/KotestPracticeApplicationTests.kt
@@ -0,0 +1,12 @@
+package me.seungpang.kotestpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotestPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}

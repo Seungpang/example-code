1e9e9e91205a1e78aba1e3ff2aed8f23c2e693cd
Seungrae <obey1342@gmail.com>
2023-06-15 18:49:37 +0900

    project init

---
+++ event-system/src/main/java/com/example/eventsystem/EventSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSystemApplication.class, args);
+    }
+
+}
---
+++ event-system/src/test/java/com/example/eventsystem/EventSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

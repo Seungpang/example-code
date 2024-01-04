3ecd798ce2ab9fffba52fd47a5a4814438d8fbcf
Seungrae <obey1342@gmail.com>
2024-01-04 17:58:36 +0900

    project init

---
+++ kafka-producer-practice/src/main/java/me/seungpang/kafkaproducerpractice/KafkaProducerPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.seungpang.kafkaproducerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerPracticeApplication.class, args);
+	}
+
+}
---
+++ kafka-producer-practice/src/test/java/me/seungpang/kafkaproducerpractice/KafkaProducerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungpang.kafkaproducerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

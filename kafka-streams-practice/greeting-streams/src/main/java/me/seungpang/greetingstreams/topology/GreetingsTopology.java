420773f6d86469073f8a400694638287894dab1c
Seungrae <obey1342@gmail.com>
2024-01-19 23:37:26 +0900

    kafka stream launcher 추가

--- kafka-streams-practice/greeting-streams/src/main/java/me/seungpang/greetingstreams/topology/GreetingsTopology.java
+++ kafka-streams-practice/greeting-streams/src/main/java/me/seungpang/greetingstreams/topology/GreetingsTopology.java
@@ -4,12 +4,13 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 
 public class GreetingsTopology {
 
-    private static final String GREETINGS = "greetings";
-    private static final String GREETINGS_UPPERCASE = "greetings_uppercase";
+    public static final String GREETINGS = "greetings";
+    public static final String GREETINGS_UPPERCASE = "greetings_uppercase";
 
     public static Topology buildTopology() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
@@ -18,9 +19,15 @@ public class GreetingsTopology {
                 stream(GREETINGS,
                         Consumed.with(Serdes.String(), Serdes.String()));
 
+        greetingsStream
+                .print(Printed.<String, String>toSysOut().withLabel("greetingsStream"));
+
         var modifiedStream = greetingsStream
                 .mapValues((readOnlyKey, value) -> value.toUpperCase());
-        
+
+        modifiedStream
+                .print(Printed.<String, String>toSysOut().withLabel("modifiedStream"));
+
         modifiedStream
                 .to(GREETINGS_UPPERCASE,
                         Produced.with(Serdes.String(), Serdes.String()));

8bee7bd290123dc809c3c66c77064bd1a6c816e3
Seungrae <obey1342@gmail.com>
2024-03-12 23:39:28 +0900

    dto를 kotlin으로 변경

--- library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -24,7 +24,7 @@ class UserService(
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse> {
         return userRepository.findAll()
-            .map { user -> UserResponse(user) }
+            .map { user -> UserResponse.of(user) }
     }
 
     @Transactional

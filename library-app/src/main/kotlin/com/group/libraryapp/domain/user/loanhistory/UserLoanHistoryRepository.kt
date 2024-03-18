c2bb489de9852141a40f46bad20a5680d815661f
Seungrae <obey1342@gmail.com>
2024-03-18 23:02:06 +0900

    책 통계 기능 추가

--- library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
+
+    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
 }

5acdb1f10af8fec3b0852b5d88a321c5f4319860
Seungrae <obey1342@gmail.com>
2024-03-23 00:13:58 +0900

    공변과 반공변

--- kotlin/src/main/kotlin/Cage.kt
+++ kotlin/src/main/kotlin/Cage.kt
@@ -1,14 +1,22 @@
 package main.kotlin
 
 fun main() {
-    val cage = Cage2<Carp>()
-    cage.put(Carp("잉어"))
-    val carp: Carp = cage.getFirst()
+//    val cage = Cage2<Carp>()
+//    cage.put(Carp("잉어"))
+//    val carp: Carp = cage.getFirst()
+//
+//    val goldFishCage = Cage2<GoldFish>()
+//    goldFishCage.put(GoldFish("금붕어"))
+//    val fishCage = Cage2<Fish>()
+//    fishCage.moveFrom(goldFishCage)
+//    val fish: Fish = fishCage.getFirst()
+
+    val fishCage = Cage2<Fish>()
 
     val goldFishCage = Cage2<GoldFish>()
     goldFishCage.put(GoldFish("금붕어"))
-    val fishCage = Cage2<Fish>()
-    //fishCage.moveFrom(goldFishCage)
+    goldFishCage.moveTo(fishCage)
+
 }
 
 class Cage {
@@ -38,7 +46,11 @@ class Cage2<T> {
         this.animals.add(animal)
     }
 
-    fun moveFrom(cage: Cage2<T>) {
+    fun moveFrom(cage: Cage2<out T>) {
         this.animals.addAll(cage.animals)
+    }
+
+    fun moveTo(otherCage: Cage2<in T>) {
+        otherCage.animals.addAll(this.animals)
     }
 }

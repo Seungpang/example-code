dbd085b465a08392af66fe9c5d64d79ddf160bd8
Seungrae <obey1342@gmail.com>
2024-04-08 23:39:24 +0900

    project init

---
+++ jpa-practices/src/main/java/me/seungpang/jpapractices/JpaPracticesApplication.java
@@ -0,0 +1,13 @@
+package me.seungpang.jpapractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPracticesApplication.class, args);
+    }
+
+}

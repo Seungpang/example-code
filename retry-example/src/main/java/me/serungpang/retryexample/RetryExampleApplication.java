eab2c64ab8ccfcb5a829ffe2d4a4260e83c21391
Seungrae <obey1342@gmail.com>
2024-04-27 18:28:53 +0900

    project init

---
+++ retry-example/src/main/java/me/serungpang/retryexample/RetryExampleApplication.java
@@ -0,0 +1,13 @@
+package me.serungpang.retryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryExampleApplication.class, args);
+	}
+
+}
---
+++ retry-example/src/test/java/me/serungpang/retryexample/RetryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.serungpang.retryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

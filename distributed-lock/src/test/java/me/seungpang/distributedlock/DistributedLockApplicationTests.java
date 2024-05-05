436f840378e077876cb3c6fa0820e1eb7b9a2b90
Seungrae <obey1342@gmail.com>
2024-05-05 23:37:54 +0900

    project init

---
+++ distributed-lock/src/test/java/me/seungpang/distributedlock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungpang.distributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

625f8c3da9a5648106b650169e3c009b63b4a577
Seungrae <obey1342@gmail.com>
2024-05-05 23:38:57 +0900

    누락된 dto 추가

---
+++ retry-example/src/main/java/me/serungpang/retryexample/service/dto/MessageLikeResponseDto.java
@@ -0,0 +1,17 @@
+package me.serungpang.retryexample.service.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class MessageLikeResponseDto {
+
+    private Long likes;
+    private Boolean liked;
+}

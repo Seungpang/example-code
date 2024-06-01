3877e5de2ddd3b2199f9d35b7648e06c1c9c727a
Seungrae <obey1342@gmail.com>
2024-06-01 17:53:27 +0900

    project init

---
+++ spring-assert/src/main/java/me/seungpang/spring_assert/SpringAssertApplication.java
@@ -0,0 +1,13 @@
+package me.seungpang.spring_assert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssertApplication.class, args);
+	}
+
+}
---
+++ spring-assert/src/test/java/me/seungpang/spring_assert/SpringAssertApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungpang.spring_assert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d7d5602cfff8785415b91e86b5ae8ffc52974c47
Seungrae <obey1342@gmail.com>
2024-06-20 22:16:27 +0900

    project init

---
+++ webClient/src/main/java/me/seungpang/webClient/WebClientApplication.java
@@ -0,0 +1,13 @@
+package me.seungpang.webClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebClientApplication.class, args);
+	}
+
+}
---
+++ webClient/src/test/java/me/seungpang/webClient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungpang.webClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
